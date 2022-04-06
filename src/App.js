a54a5a19a3d2a223bead8043bcdf0f2b2a6e0456
MariamTsikhitatrishvili <mariam.tsikhitatrishvili.1@btu.edu.ge>
2022-04-06 17:06:52 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Main from './Main';
+import Test from './Test';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename='/'>
+      <Routes>
+        <Route exact path='/' element={<Main />} />
+        <Route exact path='/home' element={<Test />} />
+      </Routes>
+    </Router>
   );
 }
 
